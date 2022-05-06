0b014df6ad37b0f9acb647bf20ab6a88ddda26d9
Ikon-design <77398364+Ikon-design@users.noreply.github.com>
2022-05-06 13:45:34 +0200

    Delete du script

--- script.sh
+++
@@ -1,7 +0,0 @@
-git clone git@github.com:Ikon-design/cube5.git
-cd cube5
-cp -r cube5/. .
-rm -rf cube5
-docker compose -f docker-compose-m.yml up -d
-#docker exec database bash -c 'mysql -uroot -p"root"' videgrenier < www/vide-grenier-en-ligne-master/sql/import.sql
-docker exec -i database mysql --user test --password=test videgrenier < www/vide-grenier-en-ligne-master/sql/import.sql

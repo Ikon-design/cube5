cd9e9d0374a561a7aa6ae71eff0ed52ff82cddab
Ikon-design <77398364+Ikon-design@users.noreply.github.com>
2022-05-06 13:19:05 +0200

    changement du script

--- script.sh
+++ script.sh
@@ -2,6 +2,6 @@ git clone git@github.com:Ikon-design/cube5.git
 cd cube5
 cp -r cube5/. .
 rm -rf cube5
-docker compose up -d
+docker compose -f docker-compose-m.yml up -d
 #docker exec database bash -c 'mysql -uroot -p"root"' videgrenier < www/vide-grenier-en-ligne-master/sql/import.sql
 docker exec -i database mysql --user test --password=test videgrenier < www/vide-grenier-en-ligne-master/sql/import.sql

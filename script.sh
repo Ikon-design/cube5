4e405e2568348acd1775d097ee9a488a859e1779
Ikon-design <77398364+Ikon-design@users.noreply.github.com>
2022-05-31 14:25:51 +0200

    Ajout des tests de articles et cities

--- script.sh
+++ script.sh
@@ -2,6 +2,7 @@ git clone git@github.com:Ikon-design/cube5.git
 cp -r cube5/. .
 rm -rf cube5
 docker compose up -d
+docker exec web php /usr/local/bin/composer require --dev phpunit/phpunit
 docker exec web php /usr/local/bin/composer install
 docker exec web rm /etc/apache2/sites-available/000-default.conf
 docker cp ./000-default.conf web:/etc/apache2/sites-available/

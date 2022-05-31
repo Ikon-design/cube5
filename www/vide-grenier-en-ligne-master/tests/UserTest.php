199665e400063af70a5c4e05d8a49935dc8c8cdc
AlexBOYERE <proalexandreboyere@gmail.com>
2022-05-31 15:08:48 +0200

    TestUnits Ok

--- www/vide-grenier-en-ligne-master/tests/UserTest.php
+++ www/vide-grenier-en-ligne-master/tests/UserTest.php
@@ -8,7 +8,7 @@ class UserTest extends TestCase
     public function testCreateUser()
     {
         $data = [
-            'username' => 'testeuh',
+            'username' => 'testeuh2',
             'email' => 'test@gmail.com',
             'password' => 'test',
             'salt' => 'test'
@@ -19,19 +19,45 @@ class UserTest extends TestCase
         return $id;
     }
 
+    public function testgetByLogin()
+    {
+        $data = [
+            'email' => 'test@gmail.com',
+        ];
+        $id = User::getByLogin($data['email']);
+        $this->assertNotEmpty($id);
+
+        return $id;
+    }
+
+    public function testgetById()
+    {
+        $data = [
+            'id' => '1',
+        ];
+        $id = User::getById($data['id']);
+        $this->assertNotEmpty($id);
+
+        return $id;
+    }
+
+    public function testcountUser()
+    {
+        $data = [
+            'is_admin' => '1',
+        ];
+        $id = User::countUser($data['is_admin']);
+        $this->assertNotEmpty($id);
+    }
+
     /**
      * @depends testCreateUser
      */
     public function testClearUser($id)
     {
-        $id = User::deleteUser($id);
-        $this->assertNotEmpty($id['id']);
+        User::deleteUser($id);
+        $this->assertNotEmpty($id);
 
         return;
     }
-
-    // public function testgetByLogin()
-    // {
-
-    // }
 }

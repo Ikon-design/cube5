72cb4aae13b820c4f44d9ffb7c6dee183f1f18e8
Antoine Bigot <62606118+NekShor@users.noreply.github.com>
2022-06-01 09:59:24 +0200

    correction

--- www/vide-grenier-en-ligne-master/public/index.php
+++ www/vide-grenier-en-ligne-master/public/index.php
@@ -9,12 +9,11 @@
 
 session_start();
 
-if(isset($_SESSION['ip'])){
+if($_SESSION['ip'] !== $_SERVER['REMOTE_ADDR']){
     session_destroy();
-}else{
-    $_SESSION['ip'] = $_SERVER['REMOTE_ADDR'];
 
 }
+
 
 /**
  * Composer
